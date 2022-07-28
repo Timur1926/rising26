df0393ac298a9c63b7f80628f80c1e7950f1b5c4
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-07-28 20:48:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TreningTimur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 37;
+            /*
+             * - Булевые переменные должны называться со вспомогательного глагола,
+             * как будто вы задаете вопрос, на который можно ответить да или нет.
+             * Или со слова, образованного от глагола.
+             * Здесь немного не понятно про впомогательный глагол.
+             */
+            bool iWantToStudy = true;
+            long populationOfTheEarth = 8_004_666_088;
+            char characterC = 'c';
+            double numberP = 3.1415926535897932384626433832795028841971693993751058209749445923078164;
+            float gravity = 9.8f;
+            string myName = "Timur";
+            sbyte airTemperature = -26;
+            short cashierSalary = 32_767;
+            ushort theSalaryOfTheMasterAtTheFactory = 65_535;
+
+        }
+    }
+}
