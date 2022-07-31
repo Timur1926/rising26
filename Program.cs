378b9e3898d775f9b5bee8565a34f3ff51f28c1e
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-07-31 15:38:06 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,19 +6,30 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** User profile ***");
-            Console.Write("Enter your name: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Enter your last name: ");
-            string lastName = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter your height: ");
-            int height = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter your weight: ");
-            double weight = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine($"Name: {firstName}\nLast name: {lastName}\nAge: {age}\nHeight: {height}см\nWeight: {weight}кг");
+            int userPictures = 54;
+            int rowNumber = 0;
+            int overTheTopPictures = 0;
+            for (int i = 1; i <= userPictures; i++)
+            {
+                //int a = 1;
+                if (i % 3 == 0)
+                {
+                    rowNumber++;
+                }
+                else
+                {
+                    if (overTheTopPictures == 2)
+                    {
+                        overTheTopPictures = 0;
+                    }
+                    overTheTopPictures++;
+                }
+            }
+            Console.WriteLine($"Заполненых рядов {rowNumber}");
+            Console.WriteLine($"Картинок сверх меры {overTheTopPictures}");
+
         }
+        
+       
     }
 }
