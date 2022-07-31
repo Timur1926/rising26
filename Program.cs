59aba613f8c8f46381b5d4e294c9a0dda9980134
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-07-31 19:29:25 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,30 +6,11 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            int userPictures = 54;
-            int rowNumber = 0;
-            int overTheTopPictures = 0;
-            for (int i = 1; i <= userPictures; i++)
-            {
-                //int a = 1;
-                if (i % 3 == 0)
-                {
-                    rowNumber++;
-                }
-                else
-                {
-                    if (overTheTopPictures == 2)
-                    {
-                        overTheTopPictures = 0;
-                    }
-                    overTheTopPictures++;
-                }
-            }
-            Console.WriteLine($"Заполненых рядов {rowNumber}");
-            Console.WriteLine($"Картинок сверх меры {overTheTopPictures}");
-
+            int userPictures = 52;
+            int fullRanks =  userPictures / 3;
+            int extraPictures = userPictures % 3;
+            Console.WriteLine($"Заполненых рядов {fullRanks}");
+            Console.WriteLine($"Картинок сверх меры {extraPictures}");
         }
-        
-       
     }
 }
