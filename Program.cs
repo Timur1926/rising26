8764c7190b415e4035b55056a6d941e0249b8cda
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-01 15:45:45 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            int userPictures = 52;
-            int numberOfColumns = 3;
-            int fullRanks =  userPictures / numberOfColumns;
-            int extraPictures = userPictures % numberOfColumns;
-            Console.WriteLine($"Заполненых рядов {fullRanks}");
-            Console.WriteLine($"Картинок сверх меры {extraPictures}");
+            Console.Write($"Введите количество золота в кошельке: ");
+            int amountOfGold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=> цена крстала = 1 золото");
+            Console.Write($"Какое количество кристалов вы хотите купить? - ");
+            int numberOfCrystals = Convert.ToInt32(Console.ReadLine());
+            int gold = amountOfGold - numberOfCrystals;
+            int userCrystals = numberOfCrystals;
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine($"Количество золота у пользователя после сделки : {gold}");
+            Console.WriteLine($"Количество кристалов у пользователя после сделки : {userCrystals}");
         }
     }
 }
