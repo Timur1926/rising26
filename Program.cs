b5191070ffcfae13735dcc03fa541a6b6e008f1d
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-01 18:38:42 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
+            int convertCrystals = 5;
             Console.Write($"Введите количество золота в кошельке: ");
             int gold = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("=> цена за 1 крстала = 5 золотых");
+            Console.WriteLine($"=> цена за 1 крстала = {convertCrystals} золотых");
             Console.Write($"Какое количество кристалов вы хотите купить? - ");
             int crystals = Convert.ToInt32(Console.ReadLine());
             int userCrystals = crystals;
-            int convertCrystals = 5;
             gold -= crystals * convertCrystals;
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine($"Количество золота у пользователя после сделки : {gold}");
