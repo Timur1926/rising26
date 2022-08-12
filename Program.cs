1794a5052b01b17c8ceda51e0e45ab30127474da
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-12 21:09:46 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace TreningTimur
+namespace _10_DifferentVariables
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            uint age = 11;
+            float accelerationOfGravity = 9.8f;
+            double numberP = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+            char percentSymbol = '%';
+            bool iWantToStudy = true;
+            int airTemperatureInWinter = -20;
+            string studentInformation = "Учусь C# на курсе \"яюниор\"";
+            byte weight = 80;
+            long goldCurrReservesRF = 574_805_146_999;
+            uint mercedesGlsPrice = 12_500_260; 
         }
     }
 }
