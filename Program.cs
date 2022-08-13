3b532b7723a7242ba8a4fdb5ecfada331957748d
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-13 16:05:47 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,23 +6,13 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
-            int age;
-            int growth;
-            double weight;
-            Console.Write("Введите ваше имя: ");
-            firstName = Console.ReadLine();
-            Console.Write("Введите вашу фамилию: ");
-            lastName = Console.ReadLine();
-            Console.Write("Введите ваш возраст: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите ваш рост: ");
-            growth = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите ваш вес: ");
-            weight = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("----------------------------");
-            Console.WriteLine($"Ваше имя {firstName} фамилия {lastName} вам {age} лет.\nВаш рост {growth}\nВаш вес {weight}");
+            int countRows;
+            int numberOfPictures = 52;
+            int countColumns = 3;
+            int extraPictures = numberOfPictures % countColumns;
+            countRows = numberOfPictures / countColumns;
+            Console.WriteLine($"Полностью заполненых рядов: {countRows}");
+            Console.WriteLine($"Лишних картинок: {extraPictures}");
         }
     }
 }
