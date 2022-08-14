4efced9b217212017d361623c1cbda465ed0ed56
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-14 16:05:49 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            int exchangeRate = 5;
-            int numberOfCrystals = 1;
-            Console.WriteLine($"Обменный курс {exchangeRate} золота = {numberOfCrystals} кристал");
+            int crystalPrice = 5;
+            int numberOfCrystals;
+            Console.WriteLine($"Обменный курс {crystalPrice} золота = 1 кристал");
             Console.Write("Введите количества золота: ");
             int amauntGold = Convert.ToInt32(Console.ReadLine());
             Console.Write("Скольео кристалов вы хотите купит?: ");
             numberOfCrystals = Convert.ToInt32(Console.ReadLine());
-            amauntGold -= numberOfCrystals * exchangeRate;
+            amauntGold -= numberOfCrystals * crystalPrice;
             Console.WriteLine($"Количество вашего золота: {amauntGold}");
             Console.WriteLine($"Количество ваших кристалов: {numberOfCrystals}");
         }
