67d15c0c03a04ed7c96eb410938052e27e7fb9eb
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-14 18:36:11 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace TreningTimur
         {
             int numberOfPeopleInLine;
             int timeForOnePerson = 10;
-            int minuts = 60;
+            int MinutesInHour = 60;
             Console.Write("Количество людей в очереди: ");
             numberOfPeopleInLine = Convert.ToInt32(Console.ReadLine());
             int waitingTimeMinutes = numberOfPeopleInLine * timeForOnePerson;
-            int waitingTimeHour = waitingTimeMinutes / minuts;
-            waitingTimeMinutes %= minuts;
+            int waitingTimeHour = waitingTimeMinutes / MinutesInHour;
+            waitingTimeMinutes %= MinutesInHour;
             Console.WriteLine($"Вы должны остаять в очереди {waitingTimeHour} часа {waitingTimeMinutes} минут");
         }
     }
