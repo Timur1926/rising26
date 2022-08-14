6ab12004093058233860ec9edbc7837a51f3917a
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-14 17:07:57 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,16 +6,15 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            int crystalPrice = 5;
-            int numberOfCrystals;
-            Console.WriteLine($"Обменный курс {crystalPrice} золота = 1 кристал");
-            Console.Write("Введите количества золота: ");
-            int amauntGold = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Скольео кристалов вы хотите купит?: ");
-            numberOfCrystals = Convert.ToInt32(Console.ReadLine());
-            amauntGold -= numberOfCrystals * crystalPrice;
-            Console.WriteLine($"Количество вашего золота: {amauntGold}");
-            Console.WriteLine($"Количество ваших кристалов: {numberOfCrystals}");
+            int numberOfPeopleInLine;
+            int timeForOnePerson = 10;
+            int hour = 60;
+            Console.Write("Количество людей в очереди: ");
+            numberOfPeopleInLine = Convert.ToInt32(Console.ReadLine());
+            int waitingTimeMinutes = numberOfPeopleInLine * timeForOnePerson;
+            int waitingTimeHour = waitingTimeMinutes / hour;
+            waitingTimeMinutes %= hour;
+            Console.WriteLine($"Вы должны остаять в очереди {waitingTimeHour} часа {waitingTimeMinutes} минут");
         }
     }
 }
