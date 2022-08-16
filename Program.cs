1c9104777e815b0a81769183447ee7a79717f747
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-16 16:49:56 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
+            int cycleStart = 5;
+            int endOfCycle = 96;
+            int iteratorChange = 7;
 
-            for (int i = 5; i <= 96; i += 7)
+            for (int i = cycleStart; i <= endOfCycle; i += iteratorChange)
             {
                 Console.Write($"{i}  ");
             }
