6c7041f3d0900986d76585318c8dbcf78db04a9a
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-16 19:01:32 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,18 +9,16 @@ namespace TreningTimur
             Random random = new Random();
             int maximumRandom = 101;
             int number = random.Next(0,maximumRandom);
-            int divider3 = 3;
-            int divider5 = 5;
+            int dividerFirst = 3;
+            int dividerSecond = 5;
             int resalt = 0;
             
             for(int i = 0; i <= number; i++)
             {
-
-                if (i % divider3 == 0 || i % divider5 == 0)
+                if (i % dividerFirst == 0 || i % dividerSecond == 0)
                 {
                     resalt += i;
                 }
-                
             }
             
             Console.WriteLine(resalt);
