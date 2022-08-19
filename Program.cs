abdca392da6defd6f7a1ae0a35ac84f80f24010e
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-19 15:30:15 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,22 +6,107 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int maximumRandom = 101;
-            int number = random.Next(0,maximumRandom);
-            int dividerFirst = 3;
-            int dividerSecond = 5;
-            int resalt = 0;
-            
-            for(int i = 0; i <= number; i++)
+            double usdRub = 60.54;
+            double usdSek = 9.07;
+            double rubUsd = 0.02;
+            double rubSek = 0.17;
+            double sekRub = 5.75;
+            double sekUsd = 0.11;
+            double yourBalanceInUsd = 10000;
+            double yourBalanceInRub = 10000;
+            double yourBalanceInSek = 10000;
+            string proceed = "8";
+            string exit = "7";
+            int countUsd;
+            int countRub;
+            int countSek;
+
+
+            Console.WriteLine("*** Обменный курс на сегодня ***");
+            Console.WriteLine("USD/RUB = 60.54  №1");
+            Console.WriteLine("USD/SEK = 9.07   №2");
+            Console.WriteLine("RUB/USD = 0.02   №3");
+            Console.WriteLine("RUB/SEK = 0.17   №4");
+            Console.WriteLine("SEK/RUB = 5.75   №5");
+            Console.WriteLine("SEK/USD = 0.11   №6");
+            Console.WriteLine("Для завершения   №7");
+            Console.WriteLine("Для продолжения  №8");
+             
+            while (proceed != exit)
             {
-                if (i % dividerFirst == 0 || i % dividerSecond == 0)
+                Console.Write("Выбери № операции из представленного выше списка: ");
+                string operation = Console.ReadLine();
+                switch (operation)
                 {
-                    resalt += i;
+                    case "1":
+                        Console.Write("Введите количество долларов: ");
+                        countUsd = Convert.ToInt32(Console.ReadLine());
+                        yourBalanceInUsd += countUsd;
+                        yourBalanceInRub -= countUsd * usdRub;
+                        Console.WriteLine("Количество USD на счёте:" + yourBalanceInUsd);
+                        Console.WriteLine("Количество RUB на счёте:" + yourBalanceInRub);
+                        Console.Write("Желаете завершить/продолжить? ");
+                        proceed = Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.Write("Введите количество долларов: ");
+                        countUsd = Convert.ToInt32(Console.ReadLine());
+                        yourBalanceInUsd += countUsd;
+                        yourBalanceInSek -= countUsd * usdSek;
+                        Console.WriteLine($"Количество долларов на счёте: {yourBalanceInUsd}");
+                        Console.WriteLine($"Количество шведских крон на счёте: {yourBalanceInSek}");
+                        Console.Write("Желаете закончить/продолжить? ");
+                        proceed = Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.Write("Введите количетсво рублей: ");
+                        countRub = Convert.ToInt32(Console.ReadLine());
+                        yourBalanceInRub += countRub;
+                        yourBalanceInUsd -= countRub * rubUsd;
+                        Console.WriteLine($"Количество рублей на счёте: {yourBalanceInRub}");
+                        Console.WriteLine($"Количество долларов на счёте: {yourBalanceInUsd}");
+                        Console.Write("Желаете закончить/продолжить? ");
+                        proceed = Console.ReadLine();
+                        break;
+                    case "4":
+                        Console.Write("Введите количетсво рублей: ");
+                        countRub = Convert.ToInt32(Console.ReadLine());
+                        yourBalanceInRub += countRub;
+                        yourBalanceInSek -= countRub * rubSek;
+                        Console.WriteLine($"Количество рублей на счёте: {yourBalanceInRub}");
+                        Console.WriteLine($"Количество шведских крон на счёте: {yourBalanceInSek}");
+                        Console.Write("Желаете закончить/продолжить? ");
+                        proceed = Console.ReadLine();
+                        break;
+                    case "5":
+                        Console.Write("Введите количетсво крон: ");
+                        countSek = Convert.ToInt32(Console.ReadLine());
+                        yourBalanceInSek += countSek;
+                        yourBalanceInRub -= countSek * sekRub;
+                        Console.WriteLine($"Количество шведских крон на счёте: {yourBalanceInSek}");
+                        Console.WriteLine($"Количество рублей на счёте: {yourBalanceInRub}");
+                        Console.Write("Желаете закончить/продолжить? ");
+                        proceed = Console.ReadLine();
+                        break;
+                    case "6":
+                        Console.Write("Введите количетсво крон: ");
+                        countSek = Convert.ToInt32(Console.ReadLine());
+                        yourBalanceInSek += countSek;
+                        yourBalanceInUsd -= countSek * sekUsd;
+                        Console.WriteLine($"Количество шведских на счёте: {yourBalanceInSek}");
+                        Console.WriteLine($"Количество долларов на счёте: {yourBalanceInUsd}");
+                        Console.Write("Желаете закончитьпродолжить? ");
+                        proceed = Console.ReadLine();
+                        break;
+                    case "7":
+                        proceed = exit;
+                        break;
                 }
             }
-            
-            Console.WriteLine(resalt);
+
+            Console.WriteLine("Баланс рублей " + yourBalanceInRub);
+            Console.WriteLine("Баланс шведсих крон " + yourBalanceInSek);
+            Console.WriteLine("Баланс долларов " + yourBalanceInUsd);
         }
     }
 }
