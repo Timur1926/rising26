82a6951712a8e8680b14292fee0a94d82eadaa57
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-21 14:31:33 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,107 +6,82 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            double usdRub = 60.54;
-            double usdSek = 9.07;
-            double rubUsd = 0.02;
-            double rubSek = 0.17;
-            double sekRub = 5.75;
-            double sekUsd = 0.11;
-            double yourBalanceInUsd = 10000;
-            double yourBalanceInRub = 10000;
-            double yourBalanceInSek = 10000;
-            string proceed = "8";
-            string exit = "7";
-            int countUsd;
-            int countRub;
-            int countSek;
+            Console.WriteLine("*** Выберите дествие из представленного ниже меню ***");
+            Console.WriteLine("Изменение цвета консоли ----------------- клавтша 1");
+            Console.WriteLine("Изменение фона консоли  ----------------- клавиша 2");
+            Console.WriteLine("Изменеение размера консоли -------------- клавтша 3"); 
+            Console.WriteLine("Темпиратура воздуха на завтра: ---------- клавиша 4");
+            Console.WriteLine("Калькулятор с 4-мя операциями +,-,/,*: -- клавиша 5");
+            Console.WriteLine("Завершить программу: ---------------------клавиша 6");
 
+            int operationNumber = 0;
+            int exit = 0;
 
-            Console.WriteLine("*** Обменный курс на сегодня ***");
-            Console.WriteLine("USD/RUB = 60.54  №1");
-            Console.WriteLine("USD/SEK = 9.07   №2");
-            Console.WriteLine("RUB/USD = 0.02   №3");
-            Console.WriteLine("RUB/SEK = 0.17   №4");
-            Console.WriteLine("SEK/RUB = 5.75   №5");
-            Console.WriteLine("SEK/USD = 0.11   №6");
-            Console.WriteLine("Для завершения   №7");
-            Console.WriteLine("Для продолжения  №8");
-             
-            while (proceed != exit)
+           
+
+            while (exit != 6)
             {
-                Console.Write("Выбери № операции из представленного выше списка: ");
-                string operation = Console.ReadLine();
-                switch (operation)
+                Console.Write($"действие №: ");
+                operationNumber = Convert.ToInt32(Console.ReadLine());
+
+                switch (operationNumber)
                 {
-                    case "1":
-                        Console.Write("Введите количество долларов: ");
-                        countUsd = Convert.ToInt32(Console.ReadLine());
-                        yourBalanceInUsd += countUsd;
-                        yourBalanceInRub -= countUsd * usdRub;
-                        Console.WriteLine("Количество USD на счёте:" + yourBalanceInUsd);
-                        Console.WriteLine("Количество RUB на счёте:" + yourBalanceInRub);
-                        Console.Write("Желаете завершить/продолжить? ");
-                        proceed = Console.ReadLine();
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
                         break;
-                    case "2":
-                        Console.Write("Введите количество долларов: ");
-                        countUsd = Convert.ToInt32(Console.ReadLine());
-                        yourBalanceInUsd += countUsd;
-                        yourBalanceInSek -= countUsd * usdSek;
-                        Console.WriteLine($"Количество долларов на счёте: {yourBalanceInUsd}");
-                        Console.WriteLine($"Количество шведских крон на счёте: {yourBalanceInSek}");
-                        Console.Write("Желаете закончить/продолжить? ");
-                        proceed = Console.ReadLine();
+                    case 2:
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
                         break;
-                    case "3":
-                        Console.Write("Введите количетсво рублей: ");
-                        countRub = Convert.ToInt32(Console.ReadLine());
-                        yourBalanceInRub += countRub;
-                        yourBalanceInUsd -= countRub * rubUsd;
-                        Console.WriteLine($"Количество рублей на счёте: {yourBalanceInRub}");
-                        Console.WriteLine($"Количество долларов на счёте: {yourBalanceInUsd}");
-                        Console.Write("Желаете закончить/продолжить? ");
-                        proceed = Console.ReadLine();
+                    case 3:
+                        Console.WindowHeight = 40;
+                        Console.WindowWidth = 60;
                         break;
-                    case "4":
-                        Console.Write("Введите количетсво рублей: ");
-                        countRub = Convert.ToInt32(Console.ReadLine());
-                        yourBalanceInRub += countRub;
-                        yourBalanceInSek -= countRub * rubSek;
-                        Console.WriteLine($"Количество рублей на счёте: {yourBalanceInRub}");
-                        Console.WriteLine($"Количество шведских крон на счёте: {yourBalanceInSek}");
-                        Console.Write("Желаете закончить/продолжить? ");
-                        proceed = Console.ReadLine();
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Random random = new Random();
+                        int temperature = random.Next(-20, 20);
+                        Console.WriteLine($"Темпиратура на завтра {temperature}");
                         break;
-                    case "5":
-                        Console.Write("Введите количетсво крон: ");
-                        countSek = Convert.ToInt32(Console.ReadLine());
-                        yourBalanceInSek += countSek;
-                        yourBalanceInRub -= countSek * sekRub;
-                        Console.WriteLine($"Количество шведских крон на счёте: {yourBalanceInSek}");
-                        Console.WriteLine($"Количество рублей на счёте: {yourBalanceInRub}");
-                        Console.Write("Желаете закончить/продолжить? ");
-                        proceed = Console.ReadLine();
+                    case 5:
+                        Console.WriteLine("Колькулятор");
+                        int number1, number2;
+                        char mathOperation;
+                        int resalt = 0;
+                        Console.Write($"Введи первое число: ");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write($"Введи второе число: ");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write($"Выбери операцию: ");
+                        mathOperation = Convert.ToChar(Console.ReadLine());
+                        switch (mathOperation)
+                        {
+                            case '+':
+                                resalt = number1 + number2;
+                                Console.WriteLine($"{number1} + {number2} = {resalt}");
+                                break;
+                            case '-':
+                                resalt = number1 + number2;
+                                Console.WriteLine($"{number1} - {number2} = {resalt}");
+                                break;
+                            case '/':
+                                resalt = number1 + number2;
+                                Console.WriteLine($"{number1} : {number2} = {resalt}");
+                                break;
+                            case '*':
+                                resalt = number1 + number2;
+                                Console.WriteLine($"{number1} * {number2} = {resalt}");
+                                break;
+                        }
                         break;
-                    case "6":
-                        Console.Write("Введите количетсво крон: ");
-                        countSek = Convert.ToInt32(Console.ReadLine());
-                        yourBalanceInSek += countSek;
-                        yourBalanceInUsd -= countSek * sekUsd;
-                        Console.WriteLine($"Количество шведских на счёте: {yourBalanceInSek}");
-                        Console.WriteLine($"Количество долларов на счёте: {yourBalanceInUsd}");
-                        Console.Write("Желаете закончитьпродолжить? ");
-                        proceed = Console.ReadLine();
-                        break;
-                    case "7":
-                        proceed = exit;
+                    case 6:
+                        exit = 6;
+                        Console.ResetColor();
+                        Console.WindowHeight = 30;
+                        Console.WindowWidth = 120;
                         break;
                 }
             }
-
-            Console.WriteLine("Баланс рублей " + yourBalanceInRub);
-            Console.WriteLine("Баланс шведсих крон " + yourBalanceInSek);
-            Console.WriteLine("Баланс долларов " + yourBalanceInUsd);
+            
         }
     }
 }
