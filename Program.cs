faab115b0996c7204c7d617abb726bd7ea40ff5e
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-08-27 22:56:47 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,85 +1,36 @@
 ﻿using System;
 
+
 namespace TreningTimur
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** Выберите дествие из представленного ниже меню ***");
-            Console.WriteLine("Изменение цвета консоли ----------------- клавтша 1");
-            Console.WriteLine("Изменение фона консоли  ----------------- клавиша 2");
-            Console.WriteLine("Изменеение размера консоли -------------- клавтша 3"); 
-            Console.WriteLine("Темпиратура воздуха на завтра: ---------- клавиша 4");
-            Console.WriteLine("Калькулятор с 4-мя операциями +,-,/,*: -- клавиша 5");
-            Console.WriteLine("Завершить программу: ---------------------клавиша 6");
+            string userName;
+            string userSymbol;
+            int iterationNumber = 2;
 
-            int operationNumber = 0;
-            int exit = 0;
+            Console.Write("Введите своё имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            userSymbol = Console.ReadLine();
 
-            while (exit != 6)
+            for (int i = 0; i <= iterationNumber; i++)
             {
-                Console.Write($"действие №: ");
-                operationNumber = Convert.ToInt32(Console.ReadLine());
-
-                switch (operationNumber)
+                if (i == 0 || i == 2)
                 {
-                    case 1:
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        break;
-                    case 2:
-                        Console.BackgroundColor = ConsoleColor.DarkYellow;
-                        break;
-                    case 3:
-                        Console.WindowHeight = 40;
-                        Console.WindowWidth = 60;
-                        break;
-                    case 4:
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Random random = new Random();
-                        int temperature = random.Next(-20, 20);
-                        Console.WriteLine($"Темпиратура на завтра {temperature}");
-                        break;
-                    case 5:
-                        Console.WriteLine("Колькулятор");
-                        int number1, number2;
-                        char mathOperation;
-                        int resalt = 0;
-                        Console.Write($"Введи первое число: ");
-                        number1 = Convert.ToInt32(Console.ReadLine());
-                        Console.Write($"Введи второе число: ");
-                        number2 = Convert.ToInt32(Console.ReadLine());
-                        Console.Write($"Выбери операцию: ");
-                        mathOperation = Convert.ToChar(Console.ReadLine());
-                        switch (mathOperation)
-                        {
-                            case '+':
-                                resalt = number1 + number2;
-                                Console.WriteLine($"{number1} + {number2} = {resalt}");
-                                break;
-                            case '-':
-                                resalt = number1 + number2;
-                                Console.WriteLine($"{number1} - {number2} = {resalt}");
-                                break;
-                            case '/':
-                                resalt = number1 + number2;
-                                Console.WriteLine($"{number1} : {number2} = {resalt}");
-                                break;
-                            case '*':
-                                resalt = number1 + number2;
-                                Console.WriteLine($"{number1} * {number2} = {resalt}");
-                                break;
-                        }
-                        break;
-                    case 6:
-                        exit = 6;
-                        Console.ResetColor();
-                        Console.WindowHeight = 30;
-                        Console.WindowWidth = 120;
-                        break;
+                    for (int j = 1; j <= userName.Length + 2; j++)
+                    {
+                        Console.Write(userSymbol);
+                    }
                 }
+                else if (i == 1)
+                {
+                    Console.Write($"{userSymbol}{userName}{userSymbol}");
+                }
+                Console.WriteLine();
             }
-            
         }
     }
 }
