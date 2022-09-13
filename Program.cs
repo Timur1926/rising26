640bf0acf49b9a7285e661a8c21536694689cda7
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-09-13 17:17:33 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,50 +6,115 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите строку скобок: ");
-            string text = Console.ReadLine();
-            int symbol = 0;
-            int count = 0;
-            int result = 0;
-            int numberEquals = 0;
-            int finishResult = 0;
+            int spellMin = 1;
+            int spellMax = 5;
+            int theAnd = 0;
+            Random random = new Random();
+            int shadowMage = 100;
+            int lightning = 25;
+            int pieceIce = 20;
+            int smallpox = 29;
+            int cure = 50;
 
-            for (int i = 0; i < text.Length; i++)
+            int boss = 100;
+            int hammer = 30;
+            int sword = 25;
+            int spear = 29;
+            int axe = 35;
+
+            while (shadowMage > theAnd && boss > theAnd)
             {
-                if (text[i] == '(')
-                {
-                    symbol++;
-                }
-                else if (text[i] == ')')
-                {
-                    if (i != text.Length - 1 && text[i + 1] != '(')
-                    {
-                        count++;
-                    }
+                int wizardStrike = random.Next(spellMin, spellMax);
+                int bossPunch = random.Next(spellMin, spellMax);
 
-                    symbol--;
+                if (shadowMage == 100 && wizardStrike == 4)
+                {
+                    wizardStrike = random.Next(spellMin, spellMax);
                 }
 
-                if (symbol < numberEquals)
+                switch (wizardStrike)
                 {
-                    break;
+                    case 1:
+                        Console.WriteLine($"Mage призывает для отаки заклинание \"молния\"");
+                        boss -= lightning;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Mage применяет заклинание \"кусок льда\"");
+                        boss -= pieceIce;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Mage использует заклинание \"чёрная оспа\"");
+                        boss -= smallpox;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Mage использует закленание \"излечение\"");
+                        shadowMage += cure;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
                 }
 
-                if (symbol == numberEquals)
+                switch (bossPunch)
                 {
-                    result = count;
+                    case 1:
+                        Console.WriteLine($"Boss наносит удар \"молотом\"");
+                        shadowMage -= hammer;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Boss бъёт \"мечём\"");
+                        shadowMage -= sword;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Boss наносит удар \"копьём\"");
+                        shadowMage -= spear;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Boss бъёт \"топором\"");
+                        shadowMage -= axe;
+                        Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
+                        break;
                 }
+
             }
-
-            if (symbol == numberEquals)
+           
+            if (shadowMage > boss)
             {
-                finishResult = result + numberEquals;
-                Console.WriteLine("Строка корректная " + text + "\n" + "Максимум глубина равняется: " + finishResult);
+                Console.WriteLine($"Победу одерживает Mage!!!!!!!!!!!!!");
+            }
+            
+            if (boss > shadowMage)
+            {
+                Console.WriteLine("Победу одеоживает Boss!!!!!!!!!!!!!!!");
             }
             else
             {
-                Console.WriteLine("Ошибка! Не верная строка " + text);
+                Console.WriteLine($"Ничья !!!!!!!!!!!!!");
             }
+
+
+            /*
+             Легенда: Вы – теневой маг(можете быть вообще хоть кем) и у вас в арсенале есть несколько заклинаний, 
+             которые вы можете использовать против Босса. Вы должны уничтожить босса и только после этого будет вам покой.
+
+             Формально: перед вами босс, у которого есть определенное кол-во жизней и определенный ответный урон. 
+             У вас есть 4 заклинания для нанесения урона боссу. Программа завершается только после смерти босса или смерти пользователя.
+
+             Пример заклинаний
+             Рашамон – призывает теневого духа для нанесения атаки (Отнимает 100 хп игроку)
+
+             Хуганзакура (Может быть выполнен только после призыва теневого духа), наносит 100 ед. урона
+
+             Межпространственный разлом – позволяет скрыться в разломе и восстановить 250 хп. Урон босса по вам не проходит
+
+             Заклинания должны иметь схожий характер и быть достаточно сложными, они должны иметь какие-то условия выполнения (пример - Хуганзакура). 
+             Босс должен иметь возможность убить пользователя.
+             */
+
         }
     }
 }
