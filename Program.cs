e30b9dcd2f8dd915c740a4f944077352e4ec6afa
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-09-15 16:21:21 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,13 +11,21 @@ namespace TreningTimur
             int number = 0;
             int mageLife = 100;
             int restorationOfLife = 4;
+            int life = 70 ;
             Random random = new Random();
             int shadowMage = 100;
             int lightning = 25;
             int pieceIce = 20;
             int smallpox = 29;
             int cure = 50;
+            int lightningSpell = 1;
+            int iceBlockSpell = 2;
+            int blackPoxSpell = 3;
             int boss = 100;
+            int hammerBlow = 1;
+            int swordStrike = 2;
+            int spearBlow = 3;
+            int axBlow = 4;
             int hammer = 30;
             int sword = 25;
             int spear = 29;
@@ -26,57 +34,59 @@ namespace TreningTimur
             while (shadowMage > number && boss > number)
             {
                 int wizardStrike = random.Next(spellMin, spellMax);
-                int bossPunch = random.Next(spellMin, spellMax);
+
 
                 if (shadowMage == mageLife && wizardStrike == restorationOfLife)
                 {
                     wizardStrike = random.Next(spellMin, spellMax);
                 }
 
-                if (wizardStrike == 1)
+                if (wizardStrike == lightningSpell)
                 {
                     Console.WriteLine($"Mage призывает для отаки заклинание \"молния\"");
                     boss -= lightning;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
-                else if (wizardStrike == 2)
+                else if (wizardStrike == iceBlockSpell)
                 {
                     Console.WriteLine($"Mage применяет заклинание \"кусок льда\"");
                     boss -= pieceIce;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
-                else if (wizardStrike == 3)
+                else if (wizardStrike == blackPoxSpell)
                 {
                     Console.WriteLine($"Mage использует заклинание \"чёрная оспа\"");
                     boss -= smallpox;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
-                else if (wizardStrike == 4)
+                else if (wizardStrike == restorationOfLife && shadowMage <= life)
                 {
                     Console.WriteLine($"Mage использует закленание \"востановления жизни\"");
                     shadowMage += cure;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
 
-                if (bossPunch == 1)
+                int bossPunch = random.Next(spellMin, spellMax);
+
+                if (bossPunch == hammerBlow)
                 {
                     Console.WriteLine($"Boss наносит удар \"молотом\"");
                     shadowMage -= hammer;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
-                else if (bossPunch == 2)
+                else if (bossPunch == swordStrike)
                 {
                     Console.WriteLine($"Boss бъёт \"мечём\"");
                     shadowMage -= sword;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
-                else if (bossPunch == 3)
+                else if (bossPunch == spearBlow)
                 {
                     Console.WriteLine($"Boss наносит удар \"копьём\"");
                     shadowMage -= spear;
                     Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
                 }
-                else if (bossPunch == 4)
+                else if (bossPunch == axBlow)
                 {
                     Console.WriteLine($"Boss бъёт \"топором\"");
                     shadowMage -= axe;
@@ -84,7 +94,7 @@ namespace TreningTimur
                 }
 
             }
-           
+
             if (shadowMage > boss)
             {
                 Console.WriteLine($"Победу одерживает Mage!!!!!!!!!!!!!");
