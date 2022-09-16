498c2217f5fd79027abeb0ee183c3a6379868c5c
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-09-16 14:52:59 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,108 +6,32 @@ namespace TreningTimur
     {
         static void Main(string[] args)
         {
-            int spellMin = 1;
-            int spellMax = 5;
-            int number = 0;
-            int mageLife = 100;
-            int restorationOfLife = 4;
-            int life = 70 ;
-            Random random = new Random();
-            int shadowMage = 100;
-            int lightning = 25;
-            int pieceIce = 20;
-            int smallpox = 29;
-            int cure = 50;
-            int lightningSpell = 1;
-            int iceBlockSpell = 2;
-            int blackPoxSpell = 3;
-            int boss = 100;
-            int hammerBlow = 1;
-            int swordStrike = 2;
-            int spearBlow = 3;
-            int axBlow = 4;
-            int hammer = 30;
-            int sword = 25;
-            int spear = 29;
-            int axe = 35;
+            int[,] mass = { { 5, 10, 15 }, { 20, 20, 20 } };
+            int resultSum = 0;
+            int rowNumber0 =0;
+            int rowNumber1 = 1;
+            int columNumber0 =0;
+            int resultMultiplication = 0;
+            Console.WriteLine("Дан следующий массив");
 
-            while (shadowMage > number && boss > number)
+            for (int i = 0; i < mass.GetLength(0); i++)
             {
-                int wizardStrike = random.Next(spellMin, spellMax);
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write(mass[i, j] + " ");
 
+                    if (i == rowNumber1)
+                        resultSum += mass[i, j];
 
-                if (shadowMage == mageLife && wizardStrike == restorationOfLife)
-                {
-                    wizardStrike = random.Next(spellMin, spellMax);
+                   
                 }
-
-                if (wizardStrike == lightningSpell)
-                {
-                    Console.WriteLine($"Mage призывает для отаки заклинание \"молния\"");
-                    boss -= lightning;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-                else if (wizardStrike == iceBlockSpell)
-                {
-                    Console.WriteLine($"Mage применяет заклинание \"кусок льда\"");
-                    boss -= pieceIce;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-                else if (wizardStrike == blackPoxSpell)
-                {
-                    Console.WriteLine($"Mage использует заклинание \"чёрная оспа\"");
-                    boss -= smallpox;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-                else if (wizardStrike == restorationOfLife && shadowMage <= life)
-                {
-                    Console.WriteLine($"Mage использует закленание \"востановления жизни\"");
-                    shadowMage += cure;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-
-                int bossPunch = random.Next(spellMin, spellMax);
-
-                if (bossPunch == hammerBlow)
-                {
-                    Console.WriteLine($"Boss наносит удар \"молотом\"");
-                    shadowMage -= hammer;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-                else if (bossPunch == swordStrike)
-                {
-                    Console.WriteLine($"Boss бъёт \"мечём\"");
-                    shadowMage -= sword;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-                else if (bossPunch == spearBlow)
-                {
-                    Console.WriteLine($"Boss наносит удар \"копьём\"");
-                    shadowMage -= spear;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-                else if (bossPunch == axBlow)
-                {
-                    Console.WriteLine($"Boss бъёт \"топором\"");
-                    shadowMage -= axe;
-                    Console.WriteLine($"Mage = {shadowMage} | {boss} = Boss");
-                }
-
+                
+                Console.WriteLine();
+                resultMultiplication = mass[rowNumber0, columNumber0] * mass[rowNumber1, columNumber0];
             }
 
-            if (shadowMage > boss)
-            {
-                Console.WriteLine($"Победу одерживает Mage!!!!!!!!!!!!!");
-            }
-            else if (boss > shadowMage)
-            {
-                Console.WriteLine("Победу одеоживает Boss!!!!!!!!!!!!!!!");
-            }
-            else
-            {
-                Console.WriteLine($"Ничья !!!!!!!!!!!!!");
-            }
-
+            Console.WriteLine($"Сумма в торой строки = {resultSum}");
+            Console.WriteLine($"Произведение первого столбца = {resultMultiplication}");
         }
     }
 }
