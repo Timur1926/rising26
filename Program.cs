ee89164f483249720b554de9a27117591bfc2102
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-09-16 20:21:51 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace TreningTimur
         {
             int[,] mass = { { 5, 10, 15 }, { 20, 20, 20 } };
             int resultSum = 0;
-            int rowNumber0 =0;
-            int rowNumber1 = 1;
-            int columNumber0 =0;
+            int rowNumber = 0;
+            int number = 1;
+            int columNumber = 0;
             int resultMultiplication = 0;
             Console.WriteLine("Дан следующий массив");
 
@@ -19,14 +19,11 @@ namespace TreningTimur
                 for (int j = 0; j < mass.GetLength(1); j++)
                 {
                     Console.Write(mass[i, j] + " ");
-
-                    if (i == rowNumber1)
-                        resultSum += mass[i, j];
-                   
                 }
                 
                 Console.WriteLine();
-                resultMultiplication = mass[rowNumber0, columNumber0] * mass[rowNumber1, columNumber0];
+                resultSum = mass[rowNumber + number, columNumber] + mass[rowNumber + number, columNumber + number] + mass[rowNumber + number, columNumber + number + number];
+                resultMultiplication = mass[rowNumber, columNumber] * mass[rowNumber + number, columNumber];
             }
 
             Console.WriteLine($"Сумма в торой строки = {resultSum}");
