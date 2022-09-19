9ec88f1409f63fbc5266911f0cb7e13e9df522ff
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-09-19 17:20:10 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,24 +8,31 @@ namespace TreningTimur
         {
             int[,] mass = { { 5, 10, 15 }, { 20, 20, 20 } };
             int resultSum = 0;
-            int rowNumber = 0;
+            int rowNumber = 1;
             int number = 1;
             int columNumber = 0;
-            int resultMultiplication = 0;
+            int resultMultiplication = mass[0,0];
+            
+
             Console.WriteLine("Дан следующий массив");
 
-            for (int i = 0; i < mass.GetLength(0); i++)
+            for ( int i = 0; i < mass.GetLength(0); i++)
             {
+
                 for (int j = 0; j < mass.GetLength(1); j++)
                 {
                     Console.Write(mass[i, j] + " ");
                 }
                 
                 Console.WriteLine();
-                resultSum = mass[rowNumber + number, columNumber] + mass[rowNumber + number, columNumber + number] + mass[rowNumber + number, columNumber + number + number];
-                resultMultiplication = mass[rowNumber, columNumber] * mass[rowNumber + number, columNumber];
             }
 
+            for (int i = 0; i <= mass.GetLength(0); i++)
+            {
+                resultSum += mass[rowNumber, i];
+            }
+
+            resultMultiplication *= mass[rowNumber, columNumber];
             Console.WriteLine($"Сумма в торой строки = {resultSum}");
             Console.WriteLine($"Произведение первого столбца = {resultMultiplication}");
         }
