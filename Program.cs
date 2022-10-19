b52e3e0cbfd1bb59522083a9923e00b4a4e2ea65
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-10-19 18:58:55 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 
-namespace SPLIT
+namespace Сортировка_чисел
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string text = "Скажика дядя ведь не даром Москва сполённая пожаром была французу отдана";
-            Console.WriteLine(text + "\n");
-            string[] arrayText = text.Split(' ');
+            int[] array = { 10, 7, 9, 5, 8, 4, 6, 2, 3, 1};
+            int value = 1;
+            int[] sortArray = new int[10];
+            Console.WriteLine("Массив");
 
-            foreach(string elementText in arrayText)
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(elementText);
+                Console.Write(array[i] + "  ");
+                
+                for (int j = 0; j < array.Length; j++)
+                {
+
+                    if (array[j] == value)
+                    {
+                        sortArray[i] = array[j];
+                    }
+                }
+
+                value++;
+            }
+
+            array = sortArray;
+            Console.WriteLine();
+            Console.WriteLine("Сортированный массив");
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "  ");
             }
 
             Console.WriteLine();
