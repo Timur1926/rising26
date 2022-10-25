790e7bf47bfa61d25a2486f19a43ccf09d43861d
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2022-10-25 20:04:01 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 
-namespace SPLIT
+namespace Сдвиг_значений_массива
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string text = "Скажика дядя ведь не даром Москва сполённая пожаром была французу отдана";
-            Console.WriteLine(text + "\n");
-            string[] arrayText = text.Split(' ');
+            int[] array = { 1, 2, 3, 4 };
+            Console.WriteLine("Сдвиг массива на n значений заданных пользователем");
 
-            foreach(string elementText in arrayText)
+            foreach (int arrayFirst in array)
             {
-                Console.WriteLine(elementText);
+                Console.Write(arrayFirst + "  ");
+            }
+
+            Console.WriteLine("\n----------");
+
+            Console.Write("Ведите значание n: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int index;
+
+            for (int i = 0; i < number; i++) 
+            {
+                index = array[0];
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                array[array.Length - 1] = index;
+            }
+
+            foreach (int arraySecond in array)
+            {
+                Console.Write(arraySecond + "  ");
             }
 
             Console.WriteLine();
