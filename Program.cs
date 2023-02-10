c098577dd29516fcb63307079ea399b389dd76d5
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2023-02-10 21:46:05 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,144 +1,48 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 
-namespace Кадровый_учёт
+namespace Healthbar
 {
     class Program
     {
         static void Main(string[] args)
         {
-            const string AddDossierCommand = "1";
-            const string AllDossierCommand = "2";
-            const string RemoveDossierCommand = "3";
-            const string SearchLastNameCommand = "4";
-            const string ExitCommand = "5";
+            Console.Write("Введите количество жизни в процентах: ");
+            int heath = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество маны в процентах: ");
+            int mana = Convert.ToInt32(Console.ReadLine());
+            DrawBar(heath, ConsoleColor.DarkRed);
+            DrawBar(mana, ConsoleColor.DarkGreen);
+        }
 
-            string[] fullName = { };
-            string[] position = { };
-            bool isContinue = true;
+        static void DrawBar(int quantity, ConsoleColor consoleColor)
+        {
+            int barValue = 100;
+            ConsoleColor defaultConsoleColor = Console.BackgroundColor;
+            char simbol = '_';
+            int percent = 0;
+            Console.Write("[");
 
-            Console.WriteLine("\tМЕНЮ");
-            Console.WriteLine($"Добавить досье - кнопка {AddDossierCommand};\nВывести всё досье - кнопака {AllDossierCommand};\n" +
-                              $"Удалить досье - кнопка {RemoveDossierCommand};\nПоиск по фамилии - кнопка {SearchLastNameCommand};\nВыход - кнопка {ExitCommand}");
-
-            while (isContinue)
+            for (int i = 1; i <= barValue; i++)
             {
-                Console.WriteLine("\t***");
-                Console.Write("Выбери номер действие из меню: ");
-                string userInput = Console.ReadLine();
 
-                switch (userInput)
+                if (i <= quantity)
                 {
-                    case AddDossierCommand:
-                        AddInformation(ref fullName, ref position);
-                        break;
-
-                    case AllDossierCommand:
-                        DisplayDossier(fullName, position);
-                        break;
-
-                    case RemoveDossierCommand:
-                        RemoveInformation(ref fullName, ref position);
-                        break;
-
-                    case SearchLastNameCommand:
-                        SearchLastName(fullName, position);
-                        break;
-
-                    case ExitCommand:
-                        isContinue = false;
-                        break;
+                    Console.BackgroundColor = consoleColor;
+                    Console.Write(simbol);
                 }
-            }
-
-            Console.WriteLine("Программа завершена !!!");
-        }
-
-        static string[] AddDossiers(string[] data)
-        {
-            string[] tempData = new string[data.Length + 1];
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                tempData[i] = data[i];
-            }
-
-            tempData[tempData.Length - 1] = Console.ReadLine();
-            data = tempData;
-            return data;
-        }
-
-        static void AddInformation(ref string[] fullName, ref string[] position)
-        {
-            Console.Write("Введите ФИО: ");
-            fullName = AddDossiers(fullName);
-            Console.Write("Введите должность: ");
-            position = AddDossiers(position);
-        }
-
-        static void DisplayDossier(string[] fullName, string[] position)
-        {
-            int rowsNumber = 1;
-
-            for (int i = 0; i < fullName.Length; i++)
-            {
-                Console.WriteLine($"{ rowsNumber}) {fullName[i]} - {position[i]}");
-                rowsNumber++;
-            }
-        }
-
-        static void RemoveInformation(ref string[] fullName, ref string[] position)
-        {
-            int verificationСonditions = 0;
-            int index = 0;
-
-            if (fullName.Length > verificationСonditions && index <= fullName.Length)
-            {
-                Console.Write("Выберите номер досье для удоления: ");
-                int numberDossier = Convert.ToInt32(Console.ReadLine());
-                index = numberDossier;
-                string[] tempFullName = fullName;
-                fullName = new string [fullName.Length - 1];
-                string[] tempPosition = position;
-                position = new string [position.Length - 1];
-                int numberIteration = 0;
-                index -= 1;
-
-                for (int i = 0; i < fullName.Length; i++)
+                else
                 {
-                    if (index > i)
-                    {
-                        fullName[i] = tempFullName[numberIteration];
-                        position[i] = tempPosition[numberIteration];
-                        numberIteration++;
-                    }
-                    else
-                    {
-                        numberIteration++;
-                        fullName[i] = tempFullName[numberIteration];
-                        position[i] = tempPosition[numberIteration];
-                    }
+                    Console.BackgroundColor = defaultConsoleColor;
+                    Console.Write(simbol);
+                    percent++;
                 }
+
             }
-        }
 
-        static void SearchLastName(string[] fullName, string[] position)
-        {
-            Console.Write("Введите фамилию для поиска досье: ");
-            string lastName = Console.ReadLine();
-            string[] name;
-            int iterationToSearch = 0;
-
-            for (int i = 0; i < fullName.Length; i++)
-            {
-                char space = ' ';
-                name = fullName[i].Split(space);
-
-                if (lastName == name[iterationToSearch])
-                {
-                    Console.WriteLine(fullName[i] + " - " + position[i]);
-                }
-            }
+            percent = barValue - percent;
+            Console.BackgroundColor = defaultConsoleColor;
+            Console.Write("]"+" "+ percent+"%");
+            Console.WriteLine();
         }
     }
 }
