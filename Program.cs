cd5c9f0d8c16efe4abda6c6d1b9cf0149d002663
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2023-02-16 15:29:35 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,22 +6,22 @@ namespace Healthbar
     {
         static void Main(string[] args)
         {
-            const int boundsCheck = 100;
-            Console.Write("Введите количество жизни в процентах: ");
-            double heathPercent = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите количество маны в процентах: ");
-            double manaPercent = Convert.ToInt32(Console.ReadLine());
+            const double barValue = 20;
+            double healthPercent = 0.0;
+            double manaPercent = 0.0;
+            bool parametersCorrect = false;
 
-            if (heathPercent <= boundsCheck && manaPercent <= boundsCheck)
+            while (parametersCorrect == false)
             {
-                DrawBar(heathPercent, 20, ConsoleColor.DarkRed);
-                DrawBar(manaPercent, 20, ConsoleColor.DarkGreen);
+                Console.Write("Введите количество жизни в процентах: ");
+                healthPercent = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите количество маны в процентах: ");
+                manaPercent = Convert.ToInt32(Console.ReadLine());
+                parametersCorrect = InterestСheck(healthPercent, manaPercent);
             }
-            else
-            {
-                Console.WriteLine("!!!ERROR!!! Попробуё ещё!");
-            }
-            
+
+            DrawBar(healthPercent, barValue, ConsoleColor.DarkRed);
+            DrawBar(manaPercent, barValue, ConsoleColor.DarkGreen);
         }
 
         static void DrawBar(double quantity, double barValue, ConsoleColor consoleColor)
@@ -52,6 +52,24 @@ namespace Healthbar
             Console.BackgroundColor = defaultConsoleColor;
             Console.Write("]" + " " + percent + "%");
             Console.WriteLine();
+        }
+
+        static bool InterestСheck(double percentHealth, double percentMana)
+        {
+            double maxValue = 100;
+            double minValue = 0;
+            bool parametersСorrect = false;
+
+            if (percentHealth <= maxValue && percentHealth >= minValue && percentMana <= maxValue && percentMana >= minValue)
+            {
+                parametersСorrect = true;
+            }
+            else
+            {
+                Console.WriteLine("!!! ERROR !!! Попробуй ещё!");
+            }
+
+            return parametersСorrect;
         }
     }
 }
