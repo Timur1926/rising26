b14d27aa160a7705b6cfbc082a7684463055a72c
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2023-02-20 12:00:03 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,58 +7,48 @@ namespace Healthbar
         static void Main(string[] args)
         {
             const double barValue = 20;
-            double healthPercent = 0.0;
-            double manaPercent = 0.0;
-            bool parametrsCorrect = true;
+            double healthPercent;
+            double manaPercent;
+            string value;
 
-            while (parametrsCorrect)
-            {
-                Console.Write("Введите количество жизни в процентах: ");
-                healthPercent = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите количество маны в процентах: ");
-                manaPercent = Convert.ToInt32(Console.ReadLine());
-                parametrsCorrect = InterestCheck(healthPercent, manaPercent);
-            }
+            Console.Write("Введите количество жизни в процентах: ");
+            value = Console.ReadLine();
+            Double.TryParse(value, out healthPercent);
+            Console.Write("Введите количество маны в процентах: ");
+            value = Console.ReadLine();
+            Double.TryParse(value, out manaPercent);
 
-            DrawBar(healthPercent, barValue, ConsoleColor.DarkRed, healthPercent, manaPercent);
-            DrawBar(manaPercent, barValue, ConsoleColor.DarkGreen, healthPercent, manaPercent);
-
+            DrawBar(healthPercent, barValue, ConsoleColor.DarkRed);
+            DrawBar(manaPercent, barValue, ConsoleColor.DarkGreen);
         }
 
-        static void DrawBar(double quantityPercent, double barValue, ConsoleColor consoleColor, double percentHealth, double percentMana)
-        {
-            ConsoleColor defaultConsoleColor = Console.BackgroundColor;
-            char simbol = '_';
-            int percent = (int)quantityPercent;
-            int allInterest = 100;
-            Console.Write("[");
-            quantityPercent = (barValue / allInterest) * quantityPercent;
-            Console.BackgroundColor = consoleColor;
-            string bar = new string(simbol, (int)quantityPercent);
-            Console.Write(bar);
-            quantityPercent = barValue - quantityPercent;
-            Console.BackgroundColor = defaultConsoleColor;
-            bar = new string(simbol, (int)quantityPercent);
-            Console.Write(bar);
-            Console.Write("]" + " " + percent + "%" + "\n");
-        }
-
-        static bool InterestCheck(double percentHealth, double percentMana)
+        static void DrawBar(double quantityPercent, double barValue, ConsoleColor consoleColor)
         {
             int barMax = 100;
             int barMin = 0;
-            bool parametrsCorrect = true;
 
-            if (percentHealth <= barMax && percentHealth >= barMin && percentMana <= barMax && percentMana >= barMin)
+            if (quantityPercent <= barMax && quantityPercent >= barMin)
             {
-                parametrsCorrect = false;
+                ConsoleColor defaultConsoleColor = Console.BackgroundColor;
+                char simbol = '_';
+                int percent = (int)quantityPercent;
+                int allInterest = 100;
+                Console.Write("[");
+                quantityPercent = (barValue / allInterest) * quantityPercent;
+                Console.BackgroundColor = consoleColor;
+                string bar = new string(simbol, (int)quantityPercent);
+                Console.Write(bar);
+                quantityPercent = barValue - quantityPercent;
+                Console.BackgroundColor = defaultConsoleColor;
+                bar = new string(simbol, (int)quantityPercent);
+                Console.Write(bar);
+                Console.Write("]" + " " + percent + "%" + "\n");
             }
             else
             {
-                Console.WriteLine("Ошибка при вводе процентов!!! Попробуй ещё!!");
+                Console.WriteLine("Начни сначала!!! Проценты не корректны!!!");
             }
 
-            return parametrsCorrect;
         }
     }
 }
