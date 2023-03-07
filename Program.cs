3f9a39527df060f3676a7608fe941094cf537b60
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2023-03-07 18:45:19 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,132 @@
 ﻿using System;
 
-namespace READINT
+namespace BraveNewWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int number;
-            number = GetReadint();
-            Console.WriteLine($"Конвертированное число = {number}");
+            DrawMap();
         }
 
-        static int GetReadint()
+        static void DrawMap()
         {
-            int number = 0;
-            bool isNumber = false;
-            string userNumber;
-
-            while (isNumber != true)
-            {
-                Console.Write("Введите чило: ");
-                userNumber = Console.ReadLine();
-                isNumber = int.TryParse(userNumber, out number);
-            }
+            Console.CursorVisible = false;
             
-            return number;
+            char[,] map =
+            {
+                {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                {'#',' ',' ',' ',' ',' ','$',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                {'#','#','#','#','#','#','#','#',' ','#','#','#','#','#','#','#','#'},
+                {'#',' ',' ',' ','$',' ',' ',' ',' ',' ','$',' ',' ',' ',' ',' ','#'},
+                {'#','#','#','#','#','#','#','#',' ','#','#','#','#','#','#','#','#'},
+                {'#',' ',' ',' ',' ',' ','$',' ',' ',' ',' ',' ',' ',' ',' ','$','#'},
+                {'#','#','#','#','#','#','#','#',' ','#','#','#','#','#','#','#','#'},
+                {'#',' ',' ',' ','$',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                {'#','#','#','#','#','#','#','#',' ','#','#','#','#','#','#','#','#'},
+                {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','$','#'},
+                {'#','#','#','#',' ','#','#','#',' ','#',' ','#','#','#','#','#','#'},
+                {'#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                {'#','$',' ','#',' ','$',' ','#',' ','$','#',' ',' ','#',' ','$','#'},
+                {'#',' ',' ','#',' ',' ',' ',' ',' ',' ','#',' ',' ','#',' ',' ','#'},
+                {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+            };
+
+            int userX = 1, userY = 1;
+            char userDog = '@';
+            char blockPlayerPosition = '#';
+            char item = '$';
+            string backpack = "";
+
+            while (true)
+            {
+
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine($"\nСобранных предметов в рюкзаке: ({backpack.Length}) - {backpack}");
+                Console.SetCursorPosition(userX, userY);
+                Console.Write(userDog);
+                ConsoleKeyInfo charKey = Console.ReadKey();
+
+                switch (charKey.Key)
+                {
+
+                    case ConsoleKey.RightArrow:
+
+                        if (map[userY, userX + 1] != blockPlayerPosition)
+                        {
+                            userX++;
+                            
+                            if (map[userY, userX] == item)
+                            {
+                                backpack += item;
+                                map[userY, userX] = ' ';
+                            }
+
+                        }
+
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+
+                        if (map[userY, userX - 1] != blockPlayerPosition)
+                        {
+                            userX--;
+
+                            if (map[userY, userX] == item)
+                            {
+                                backpack += item;
+                                map[userY, userX] = ' ';
+                            }
+
+                        }
+
+                        break;
+
+                    case ConsoleKey.UpArrow:
+
+                        if (map[userY - 1, userX] != blockPlayerPosition)
+                        {
+                            userY--;
+
+                            if (map[userY, userX] == item)
+                            {
+                                backpack += item;
+                                map[userY, userX] = ' ';
+                            }
+
+                        }
+
+                        break;
+
+                    case ConsoleKey.DownArrow:
+
+                        if (map[userY + 1, userX] != blockPlayerPosition)
+                        {
+                            userY++;
+
+                            if (map[userY, userX] == item)
+                            {
+                                backpack += item;
+                                map[userY, userX] = ' ';
+                            }
+
+                        }
+
+                        break;
+                }
+
+                Console.Clear();
+            }
         }
     }
 }
