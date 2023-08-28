3c5441700934aa53430fa099324a50832429ef6f
Timur1926 <110040372+Timur1926@users.noreply.github.com>
2023-08-28 22:19:12 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,34 +6,35 @@ namespace WorkingWithProperties
     {
         static void Main(string[] args)
         {
-            Player simbolPlayer = new Player();
-            Draw payer1 = new Draw(5,2);
-            payer1.ShowPlayer(simbolPlayer.simbolPlayer);
-            Draw player2 = new Draw(10, 5);
-            player2.ShowPlayer(simbolPlayer.simbolPlayer);
+            Draw drawPlayer = new Draw();
+            drawPlayer.ShowPlayer();
         }
     }
 
     class Draw
     {
-        protected int _positionX;
-        protected int _positionY;
+        Player player = new Player();
 
-        public Draw(int positionX, int positionY)
+        public void ShowPlayer()
         {
-            _positionX = positionX;
-            _positionY = positionY;
-        }
-        
-        public void ShowPlayer(char playerSimbol)
-        {
-            Console.SetCursorPosition(_positionX, _positionY);
-            Console.WriteLine(playerSimbol);
+            Console.SetCursorPosition(player.PositionX, player.PositionY);
+            Console.WriteLine(player.SimbolPlayer);
         }
     }
 
     class Player 
     {
-        public char simbolPlayer = '$';
+        public char SimbolPlayer = '$';
+        public int PositionX = 5;
+        public int PositionY = 2;
+
+        public Player() { }
+
+        public Player (int positionX,  int positionY, char simbolPlayer)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            SimbolPlayer = simbolPlayer;
+        }
     }
 }
